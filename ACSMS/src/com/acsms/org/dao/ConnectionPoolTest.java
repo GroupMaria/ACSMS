cf080e04a47b7a52e537a4e70371cbc0e14262f1
Phidza <mablazo@hotmail.com>
2015-02-28 17:07:25 -0500

    Connection Pool Coding Signed-off-by: Phidza <mablazo@hotmail.com>

---
+++ ACSMS/src/com/acsms/org/dao/ConnectionPoolTest.java
@@ -0,0 +1,26 @@
+package com.acsms.org.dao;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectionPoolTest {
+
+	public static void main(String[] args) {
+		// TODO Change to JUnit
+		
+		ConnectionPool tset=new ConnectionPool();
+		try {
+			
+			Statement statement = tset.getConnect();
+		      // Result set get the result of the SQL query
+		      ResultSet resultSet = statement
+		          .executeQuery("select * from acsms.quotation");
+		      System.out.println(resultSet);
+		      tset.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
--- ACSMS/src/com/acsms/org/dao/QuotationDao.java
+++ ACSMS/src/com/acsms/org/dao/QuotationDao.java
@@ -14,12 +14,16 @@ public class QuotationDao {
 	
 	
 	private QuotationVO quotation;
+	private ConnectionPool connection;
+	
 	public QuotationDao(QuotationVO quotation) {
 		this.quotation=quotation;
 	}
 	public void createNewQuote() {
 		// Database Code
 		quotation.getCustomer();
+		connection=new ConnectionPool();
+		
 		
 		
 	}
