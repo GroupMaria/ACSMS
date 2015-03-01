474ac03b4b5650510f870e5a1d52f91926bcb49a
imraangithub <aimraan8@gmail.com>
2015-03-01 12:09:44 -0500

    Revert "Signed-off-by: imraangithub <aimraan@gmail.com>"
    This reverts commit 26aac75bdcfa185ae451bd6ca8cf478f38dd31f3.

--- ACSMS/src/com/acsms/org/ba/QuotationAction.java
+++ ACSMS/src/com/acsms/org/ba/QuotationAction.java
@@ -4,16 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.acsms.org.dao.OrderDetailsDao;
 import com.acsms.org.dao.QuotationDao;
-import com.acsms.org.vo.OrderDetailsVO;
 import com.acsms.org.vo.QuotationVO;
 
 /**
@@ -22,76 +19,71 @@ import com.acsms.org.vo.QuotationVO;
 @WebServlet("/QuotationAction")
 public class QuotationAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuotationAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
-	 * @see HttpServlet#HttpServlet()
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	public QuotationAction() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Set response content type
+	      response.setContentType("text/html");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	      PrintWriter out = response.getWriter();
+		  String title = "Reading All Form Parameters";
+	      String docType =
+	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+	      "transitional//en\">\n";
+	      out.println(docType +
+	    	        "<html>\n" +
+	    	        "<head><title>" + title + "</title></head>\n" +
+	    	        "<body bgcolor=\"#f0f0f0\">\n" +
+	    	        "<h1 align=\"center\">" + title + "</h1>\n" +
+	    	        "<table width=\"100%\" border=\"1\" align=\"center\">\n" +
+	    	        "<tr bgcolor=\"#949494\">\n" +
+	    	        "<th>Param Name</th><th>Param Value(s)</th>\n"+
+	    	        "</tr>\n");
 
-		QuotationVO quotation = new QuotationVO();
+	    	      Enumeration paramNames = request.getParameterNames();
+	    	      
+	    	      while(paramNames.hasMoreElements()) {
+	    	         String paramName = (String)paramNames.nextElement();
+	    	         out.print("<tr><td>" + paramName + "</td>\n<td>");
+	    	         String[] paramValues =
+	    	                request.getParameterValues(paramName);
+	    	         // Read single valued data
+	    	         if (paramValues.length == 1) {
+	    	           String paramValue = paramValues[0];
+	    	           if (paramValue.length() == 0)
+	    	             out.println("<i>No Value</i>");
+	    	           else
+	    	             out.println(paramValue);
+	    	         } else {
+	    	             // Read multiple valued data
+	    	             out.println("<ul>");
+	    	             for(int i=0; i < paramValues.length; i++) {
+	    	                out.println("<li>" + paramValues[i]);
+	    	             }
+	    	             out.println("</ul>");
+	    	         }
+	    	      }
+	    	      out.println("</tr>\n</table>\n</body></html>");
+	      
+		QuotationVO quotation=new QuotationVO();
 		quotation.setDatePrepared(request.getParameter("DatePrepared"));
 		quotation.setProduct(request.getParameter("product"));
 		quotation.setCustomer(request.getParameter("customer"));
-		quotation.setShippingFrom(request.getParameter("ShippingFrom"));
-		quotation.setDestinationCountry(request
-				.getParameter("destinationCountry"));
-		quotation.setTermsOfPayment(request.getParameter("termsOfPayment"));
-		quotation.setModeofTransport(request.getParameter("modeofTransport"));
-		quotation.setRefNumber(request.getParameter("refNumber"));
-		quotation.setEstimatedShipDate(request
-				.getParameter("EstimatedShipDate"));
-		quotation.setPackedDimensions(request.getParameter("packedDimensions"));
-		quotation.setShippingTo(request.getParameter("ShippingTo"));
-		quotation.setPackedWeight(request.getParameter("packedWeight"));
-		quotation.setPackedCube(request.getParameter("packedCube"));
-		quotation.setProductSellingPrice(request
-				.getParameter("productSellingPrice"));
-		quotation.setProductSellingPriceQuote(request
-				.getParameter("productSellingPriceQuote"));
-		quotation.setSellingPriceQuotedBy(request
-				.getParameter("sellingPriceQuotedBy"));
-		quotation.setInlandFreight(request.getParameter("inlandFreight"));
-		quotation.setInlandFreightQuote(request
-				.getParameter("inlandFreightQuote"));
-		quotation.setInlandFreightQuotedBy(request
-				.getParameter("inlandFreightQuotedBy"));
-		quotation.setInlandFreightothers(request
-				.getParameter("inlandFreightothers"));
-		quotation.setInternationalFreightQuote1(request
-				.getParameter("InternationalFreightQuote1"));
-		quotation.setInternationalFreightQuotedBy(request
-				.getParameter("InternationalFreightQuotedBy"));
-		quotation.setForwardingFees(request.getParameter("ForwardingFees"));
-		quotation.setConsularPrice(request.getParameter("ConsularPrice"));
-		quotation.setInspectionFees(request.getParameter("InspectionFees"));
-		quotation.setBankingCharges(request.getParameter("BankingCharges"));
-		quotation.setOtherCharges(request.getParameter("OtherCharges"));
-		quotation.setOtherCharge1(request.getParameter("OtherCharge1"));
-		quotation.setOtherCharge1Name(request.getParameter("OtherCharge1Name"));
-		quotation.setOtherCharge2(request.getParameter("OtherCharge2"));
-		quotation.setOtherCharge2Name(request.getParameter("OtherCharge2Name"));
-		quotation.setTotalExportquotation("");
-
-		QuotationDao quoteDao = new QuotationDao(quotation);
+		
+		QuotationDao quoteDao=new QuotationDao(quotation);
 		quoteDao.createNewQuote();
-
-		request.setAttribute("QuoteDetails", quotation);
-
-		RequestDispatcher view = request
-				.getRequestDispatcher("/intermediate.jsp");
-
-		view.forward(request, response);
-
+		
 	}
 
 }
