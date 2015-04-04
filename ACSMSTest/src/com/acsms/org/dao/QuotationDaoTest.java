6377ca8722356e54c94de78f1117e54b75447f76
evlynmaria <evlyn.maria.peter@gmail.com>
2015-04-04 15:41:03 -0400

    Signed-off-by: evlynmaria <evlyn.maria.peter@gmail.com>

---
+++ ACSMSTest/src/com/acsms/org/dao/QuotationDaoTest.java
@@ -0,0 +1,442 @@
+package com.acsms.org.dao;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import com.acsms.org.vo.QuotationVO;
+import com.acsms.org.vo.QuotationVOFactory;
+
+/**
+ * The class <code>QuotationDaoTest</code> contains tests for the class <code>{@link QuotationDao}</code>.
+ *
+ * @generatedBy CodePro at 04/04/15 3:34 PM
+ * @author Hp
+ * @version $Revision: 1.0 $
+ */
+public class QuotationDaoTest {
+	/**
+	 * Run the QuotationDao(QuotationVO) constructor test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testQuotationDao_1()
+		throws Exception {
+		QuotationVO quotation = QuotationVOFactory.createQuotationVO();
+
+		QuotationDao result = new QuotationDao(quotation);
+
+		// add additional test code here
+		assertNotNull(result);
+	}
+
+	/**
+	 * Run the QuotationDao(QuotationVO) constructor test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testQuotationDao_2()
+		throws Exception {
+		QuotationVO quotation = QuotationVOFactory.createQuotationVO();
+
+		QuotationDao result = new QuotationDao(quotation);
+
+		// add additional test code here
+		assertNotNull(result);
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_1()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_2()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_3()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_4()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_5()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_6()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_7()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_8()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_9()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_10()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_11()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_12()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_13()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_14()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_15()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Run the void createNewQuote() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Test
+	public void testCreateNewQuote_16()
+		throws Exception {
+		QuotationDao fixture = new QuotationDao(QuotationVOFactory.createQuotationVO());
+
+		fixture.createNewQuote();
+
+		// add additional test code here
+		// An unexpected exception was thrown in user code while executing this test:
+		//    java.lang.NullPointerException
+		//       at java.text.SimpleDateFormat.parse(Unknown Source)
+		//       at java.text.DateFormat.parse(Unknown Source)
+		//       at com.acsms.org.dao.QuotationDao.createNewQuote(QuotationDao.java:67)
+	}
+
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 04/04/15 3:34 PM
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(QuotationDaoTest.class);
+	}
+}
