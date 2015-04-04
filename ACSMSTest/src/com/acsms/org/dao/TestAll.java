9c89ccb6af13a0d3453e782e55cdfd5ed140ec61
evlynmaria <evlyn.maria.peter@gmail.com>
2015-04-04 14:24:24 -0400

    Revert "Revert "Junit Test Cases Updated""
    This reverts commit 4468d80c1815a9970e97f515759aef1b42dcccfe.

--- ACSMSTest/src/com/acsms/org/dao/TestAll.java
+++ ACSMSTest/src/com/acsms/org/dao/TestAll.java
@@ -9,15 +9,17 @@ import org.junit.runners.Suite;
  * of the tests within its package as well as within any subpackages of its
  * package.
  *
- * @generatedBy CodePro at 28/03/15 3:42 PM
+ * @generatedBy CodePro at 31/03/15 2:48 PM
  * @author Hp
  * @version $Revision: 1.0 $
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	ReportingDaoTest.class,
+	TransportBookingDAOTest.class,
 	StaffDaoTest.class,
 	CustomerDaoTest.class,
+	QuotationDaoTest.class,
 	OrderDetailsDaoTest.class,
 })
 public class TestAll {
@@ -27,7 +29,7 @@ public class TestAll {
 	 *
 	 * @param args the command line arguments
 	 *
-	 * @generatedBy CodePro at 28/03/15 3:42 PM
+	 * @generatedBy CodePro at 31/03/15 2:48 PM
 	 */
 	public static void main(String[] args) {
 		JUnitCore.runClasses(new Class[] { TestAll.class });
