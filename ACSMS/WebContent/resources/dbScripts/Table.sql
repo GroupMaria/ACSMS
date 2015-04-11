3cb10954c21042bbf388764e3598ccdd8f24f887
Adam Zhao <adamtieao@gmail.com>
2015-04-11 16:40:55 -0400

    user table

--- ACSMS/WebContent/resources/dbScripts/Table.sql
+++ ACSMS/WebContent/resources/dbScripts/Table.sql
@@ -123,4 +123,14 @@ CREATE TABLE `order` (
   PRIMARY KEY (`clearance_id`)  
     );
 
+    
+CREATE TABLE `acsms`.`user` (
+ `id` INT NOT NULL AUTO_INCREMENT,
+ `username` VARCHAR(45) NOT NULL,
+ `password` VARCHAR(45) NOT NULL,
+ PRIMARY KEY (`id`));
+
+ALTER TABLE `acsms`.`user` 
+ADD UNIQUE INDEX `username_UNIQUE` (`username` ASC);
+
   
